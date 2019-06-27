bfbeecb8874057a862ffd9c74ac21a305a830e7c
Cadiz <shiena.kaye.cadiz@accenture.com>
2019-06-27 19:44:48 +0800

    i18n

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -7,11 +7,24 @@ sap.ui.define([
 		metadata : {
 		    manifest: "json",
 		    config: {
-		    	"fullWidth": "true"
+		    	"fullWidth": "true",
+		    	"bundleName": "fioritraining/i18n/i18n.properties"
 		    }
 		},
 		init: function() {
 			UIComponent.prototype.init.apply(this, arguments)
+			
+			var oRootPath = jQuery.sap.getModulePath("fioritraining");
+			var mConfig = this.getMetadata().getConfig();
+			
+			this.initializei18nModel(oRootPath, mConfig);
+		},
+		
+		initializei18nModel: function(oRootPath, mConfig) {
+			var i18nModel = new sap.ui.model.resource.ResourceModel({
+				bundleUrl : [oRootPath, mConfig.bundleName].join("/")
+			});
+			this.setModel(i18nModel, "i18n");
 		}
 	});
 });
